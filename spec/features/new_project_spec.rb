271be9f3d32e33297e488e36edce38b63de5194a
Zach Boatrite <zach@tenforwardconsulting.com>
2016-04-08 15:33:24 -0500

    Add mailer settings to dev/production

--- spec/features/new_project_spec.rb
+++ spec/features/new_project_spec.rb
@@ -44,4 +44,41 @@ RSpec.describe "Creating a new project" do
       expect(File).to exist "#{project_path}/spec/shared_examples/model_policy_shared_examples.rb"
     end
   end
+
+  context 'mailer settings' do
+    let(:code) { <<-CODE.strip_heredoc
+
+          # Mailer config
+          config.action_mailer.perform_deliveries = true
+          config.action_mailer.default_url_options = { host: 'something'}
+          config.action_mailer.asset_host = 'something'
+          config.action_mailer.smtp_settings = {
+            :address   => "smtp.mandrillapp.com",
+            :port      => 25,                       # ports 587 and 2525 are also supported with STARTTLS
+            :enable_starttls_auto => true,          # detects and uses STARTTLS
+            :user_name => ENV["MANDRILL_USERNAME"],
+            :password  => ENV["MANDRILL_PASSWORD"], # SMTP password is any valid API key
+            :authentication => 'login',             # Mandrill supports 'plain' or 'login'
+            :domain => 'something',                 # your domain to identify your server when connecting
+          }
+        end
+      CODE
+    }
+
+    it 'adds mailer config to config/environments/production.rb' do
+      file = File.read "#{project_path}/config/environments/production.rb"
+      expect(file).to end_with code
+    end
+
+    it 'adds mailer config to config/environments/dev.rb' do
+      file = File.read "#{project_path}/config/environments/dev.rb"
+      expect(file).to end_with code
+    end
+
+    it 'adds keys to application.yml.example' do
+      file = File.read "#{project_path}/config/application.yml.example"
+      expect(file).to include 'MANDRILL_USERNAME:'
+      expect(file).to include 'MANDRILL_PASSWORD:'
+    end
+  end
 end
